1ee70cb56629bd7527ee44601340cfab685ba423
Luis Maldonado <maldonadovla@gmail.com>
2019-02-11 11:48:08 -0700

    product and products query for Product type

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,8 +9,21 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const products =  () => {
+  return Product.find({}).exec()
+}
+
+const product = ( _, arg) => {
+  return Product.findById(arg.id)
+    .lean()
+    .exec()
+}
+
 export default {
-  Query: {},
+  Query: {
+    products,
+    product
+  },
   Mutation: {},
   Product: {
     __resolveType(product) {}
