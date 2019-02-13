74e09719b859514b1e70a23693b62ee3edeb786a
Luis Maldonado <maldonadovla@gmail.com>
2019-02-13 12:59:22 -0700

    Solved lesson 3. Complete schema and resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,7 +9,7 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
-const products =  () => {
+const products = () => {
   return Product.find({}).exec()
 }
 
@@ -19,13 +19,41 @@ const product = ( _, arg) => {
     .exec()
 }
 
+const newProduct = (_, args, ctx) => {
+  return Product.create({ ...args.input, createdBy: ctx.user._id })
+}
+
+const updateProduct = (_, args) => {
+  return Product.findByIdAndUpdate(args.id, args.input, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args) => {
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
+
 export default {
   Query: {
     products,
     product
   },
-  Mutation: {},
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      return User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
+
+
+
